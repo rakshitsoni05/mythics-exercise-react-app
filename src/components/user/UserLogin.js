1904aaefe6fafb344d6433e2d3bc7d5ba5f7200c
rakshitsoni05 <rakshitsoni5@gmail.com>
2023-04-27 00:23:14 -0400

    changes for netlify

--- src/components/user/UserLogin.js
+++ src/components/user/UserLogin.js
@@ -1,11 +1,13 @@
 import {useState} from "react";
 import {useAuth0} from "@auth0/auth0-react";
+import {useNavigate} from "react-router-dom";
 
 const UserLogin = () => {
 
     const [emailId, setEmailId] = useState('');
     const {logout, isAuthenticated} = useAuth0();
     const {isLoading, error} = useAuth0();
+    const navigate = useNavigate();
 
     const {loginWithRedirect} = useAuth0();
 
