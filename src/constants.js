1b18676f9878ce4eb4f20fc6f222230faeb790cc
rakshitsoni05 <rakshitsoni5@gmail.com>
2023-04-26 23:48:38 -0400

    added render link

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const APP_URL = {
     react: 'http://localhost:3000',
-    node_server: 'http://localhost:4000'
+    node_server: 'https://mythics-node-service.onrender.com'
 };
 
 export const domain = 'dev-3aqyf2relxvmdq8d.us.auth0.com';
